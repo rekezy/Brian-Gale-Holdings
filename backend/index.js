b7812aa90291808de1c3defdbe12f7b3cc4cb46f
curtiscrentsil <56942550+curtiscrentsil@users.noreply.github.com>
2024-07-07 09:24:40 +0100

    register done

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const User = require("./schema/User");
-const cors = require('cors')
-const bcrypt = require('bcrypt')
-
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const uri =
   "mongodb+srv://curtis:4hsEqN4C1TX4MY2g@cluster0.momr3qu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -12,10 +11,7 @@ const app = express();
 const port = process.env.Port || 8000;
 
 app.use(bodyParser.json());
-
-
-
-app.use(cors())
+app.use(cors());
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,14 +24,22 @@ mongoose
   });
 
 app.post("/register", async (req, res) => {
-  console.log(req)
-  const { firstName, lastName, email, phone, password, country, referralCode, userName} =
-    req.body;
-    if (!userName || !email || !password) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
-    }
-    const existingUser = await User.findOne({ email });
-    
+  console.log(req.body);
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    country,
+    referralCode,
+    userName,
+  } = req.body;
+  if (!userName || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  const existingUser = await User.findOne({ email });
+
   if (!existingUser) {
     const user = new User({
       firstName: firstName,
@@ -46,8 +50,7 @@ app.post("/register", async (req, res) => {
       country,
       referralCode,
       userName,
-      isBusiness: false
-      
+      isBusiness: false,
     });
 
     try {
@@ -61,30 +64,30 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
   }
-    try {
-      // Find the user by email
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      console.log(password, email, user.password)
-      // Compare the provided password with the stored hashed password
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      }
-  
-      // If the password matches, authentication is successful
-      res.status(200).json({ message: 'Login successful', user });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
+    console.log(password, email, user.password);
+    // Compare the provided password with the stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // If the password matches, authentication is successful
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.get("/user", async (req, res) => {
   const result = await User.find();
@@ -93,11 +96,12 @@ app.get("/user", async (req, res) => {
   res.send(result);
 });
 
-app.get("user/:id" , async (req, res) => {
+app.get("user/:id", async (req, res) => {
   const result = await User.findById(req.params.id);
   //   const result = await User.findOne({ email: "curts@gmail.com" });
   console.log(result);
-  res.send(result); })
+  res.send(result);
+});
 
 app.listen(port, () => {
   console.log(`App listening  on ${port}`);
--- js/login.js
+++ js/login.js
@@ -1,85 +1,79 @@
+document.getElementById("login_button").addEventListener("click", () => {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
 
+  console.log("hello");
 
-document.getElementById('login_button').addEventListener('click', () => {
-       const email = document.getElementById('email')
-       const password = document.getElementById('password')
+  console.log(email.value, password.value);
 
-      console.log('hello')
+  fetch("http://localhost:8000/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.user) {
+        document.cookie = `Bra**&&__hold=${data.user._id}; expires=Thu, 31 Dec 2099 23:59:59 GMT; path=/`;
+        location.href =
+          "./Dashboard/html/template/vertical-light/main/index2.html";
+      } else {
+        alert(data.message);
+      }
+    });
+});
 
-       console.log(email.value, password.value)
-
-
-
-       fetch('http://localhost:8000/login',  { 
-       method: 'POST',
-       body:JSON.stringify({
-              email: email.value,
-              password: password.value
-       
-       }),
-       headers:{
-       "Content-Type":"application/json",},
-       })
-       .then((res) => res.json())
-       .then((data) => {
-              console.log(data)
-              if (data.user) {
-
-                     document.cookie =`Bra**&&__hold=${data.user._id}; expires=Thu, 31 Dec 2099 23:59:59 GMT; path=/`;
-                     location.href = "./Dashboard/html/template/vertical-light/main/index2.html"
-              } else {
-                     alert(data.message)
-              }
-       })
-
-
-})
-
-
-
- document.getElementById('business_register').addEventListener('click ', () => {
-      
-       const name = document.getElementById('name').value;
-       const businessName = document.getElementById('business_name').value;
-       const businessNumber = document.getElementById('business_number').value;
-       const email = document.getElementById('email').value;
-       const password = document.getElementById('password').value;
-       alert(`Registered with Username: ${username}, Email: ${email} password: ${password}`);
-       console.log(`Registered with Username: ${username}, Email: ${email} password: ${password}`);
-       console.log('Hello')
-   });
+document.getElementById("business_register").addEventListener("click ", () => {
+  const name = document.getElementById("name").value;
+  const businessName = document.getElementById("business_name").value;
+  const businessNumber = document.getElementById("business_number").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  alert(
+    `Registered with Username: ${username}, Email: ${email} password: ${password}`
+  );
+  console.log(
+    `Registered with Username: ${username}, Email: ${email} password: ${password}`
+  );
+  console.log("Hello");
+});
 
 //document.getElementById('').addEventListener('click', async (e) => {
-     //  e.preventDefault();
-      // const firstName =document.getElementById('firstName').value
-      // const referral =document.getElementById('referral').value
-      // const username = document.getElementById('Username').value;
-      // const lastName =document.getElementById('lastName').value
-      // const pin =document.getElementById('pin').value
-      // const email = document.getElementById('Email').value;
-      // const password = document.getElementById('Password').value;
+//  e.preventDefault();
+// const firstName =document.getElementById('firstName').value
+// const referral =document.getElementById('referral').value
+// const username = document.getElementById('Username').value;
+// const lastName =document.getElementById('lastName').value
+// const pin =document.getElementById('pin').value
+// const email = document.getElementById('Email').value;
+// const password = document.getElementById('Password').value;
 
-     //  console,log('hello')
-   
-     //  console.log(firstName.value, referral.value, username.value, lastName, pin, email, password)
+//  console,log('hello')
 
-     //  try {
-             // const response = await fetch('http://localhost:8000/register', {
-              //    method: 'POST',
-             //     headers: { 'Content-Type': 'application/json' },
-            //      body: JSON.stringify({ username, email, password, firstName, referral, username, lastName,pin })
-            //  });
-      
-             // const data = await response.json();
-      
-            //  if (response.ok) {
-              //    localStorage.setItem('token', data.token);
-             //     window.location.href = 'dashboard.html';
-            //  } else {
-              //    alert(data.msg);
-             // }
-         // } catch (error) {
-            //  console.error('Error:', error);
-        //  }
-     // });
-      
+//  console.log(firstName.value, referral.value, username.value, lastName, pin, email, password)
+
+//  try {
+// const response = await fetch('http://localhost:8000/register', {
+//    method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//      body: JSON.stringify({ username, email, password, firstName, referral, username, lastName,pin })
+//  });
+
+// const data = await response.json();
+
+//  if (response.ok) {
+//    localStorage.setItem('token', data.token);
+//     window.location.href = 'dashboard.html';
+//  } else {
+//    alert(data.msg);
+// }
+// } catch (error) {
+//  console.error('Error:', error);
+//  }
+// });
